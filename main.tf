6418715eeadeb3a327af88eb1744c9021e84bdd9
NotMaharshi <maharshi.bhatnagar@clouddrove.com>
2026-07-03 02:49:18 +0530

    fix: provider name

--- main.tf
+++ main.tf
@@ -7,10 +7,40 @@ output "broken" {
 }
 
 locals {
-  missing_value = var.values + "abc"
+  missing_value = var.values + 10
 }
 
 variable "values" {
-  type = number
+  type    = number
   default = 1
+}
+
+resource "azurerm_resource_group" "test-rg" {
+  for_each = { for i in var.map-test : i.name => i }
+  name     = each.value.name
+  location = each.value.location
+}
+
+variable "map-test" {
+  type = list(object({
+    name     = string
+    location = string
+    env      = string
+  }))
+
+  default = [{
+    name     = "test-rg"
+    location = "eastus"
+    env      = "dev"
+  }]
+
+}
+
+
+output "rg-name" {
+  value = {for k,  v in azurerm_resource_group.test-rg : k => v.name}
+}
+
+output "rg-location" {
+  value = {for k,  v in azurerm_resource_group.test-rg : k => v.default}
 }
